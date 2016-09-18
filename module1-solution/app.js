282d9eae0bb6893f6b07ff1e230cad007fed6ae1
Radu Oprita <oprita@gmail.com>
2016-09-18 00:45:51 -0400

    finish project

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,10 +4,24 @@
     angular.module('LunchCheck', [])
         .controller('LunchCheckController', LunchCheckController);
 
-    LunchCheckController.$inject('$scope');
+    LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
-        $scope.name = "Yaakov";
+        $scope.lunchItems = "";
 
+        $scope.quantityChecker = function () {
+            var items = $scope.lunchItems.split(',');
+            var itemQuantity = items.length;
+            var msg = "";
+
+            if(items[0] == "") {
+                msg = "Please enter data first";
+            } else if(itemQuantity <= 3) {
+                msg = "Enjoy!";
+            } else {
+                msg = "Too much!";
+            }
+            $scope.message = msg;
+        }
     }
 
 })();
